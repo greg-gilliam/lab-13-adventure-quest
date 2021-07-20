0d07403ce855e26dfbb05df0d55b9d7cda0e8c4d
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-07-20 13:47:12 -0700

    removed some consoleLogs

--- quest/quest.js
+++ quest/quest.js
@@ -2,14 +2,13 @@ import neighborhoods from '../data/neighborhood-data.js';
 import findById from '../data/find-by-id.js';
 
 const searchParams = new URLSearchParams(window.location.search);
-console.log(searchParams);
+
 const questTitle = document.getElementById('quest-title');
 const questImage = document.getElementById('quest-image');
 const questDescription = document.getElementById('quest-description');
 const choices = document.getElementById('choices');
 
 const quest = findById(neighborhoods, searchParams.get('questId'));
-console.log(neighborhoods, searchParams.get('questId'));
 
 questTitle.textContent = quest.title;
 questImage.src = `../assets/quests/${quest.image}`;

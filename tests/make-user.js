e3a9dfdedfa4755f4b8e8b4bade9b3d78d7db49e
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-07-20 15:19:30 -0700

    updates

---
+++ tests/make-user.js
@@ -0,0 +1,20 @@
+// import { makeUser } from '../home/make-user.js';
+// const test = QUnit.test;
+
+// test('should create a new user', (expect) => {
+//     const expected = {
+//         name: 'Greg',
+//         costume: 'ghost',
+//         health: 35,
+//         candy: 0,
+//         completed: {}
+//     };
+
+//     const userData = new FormData();
+//     userData.set('name', 'Greg');
+//     userData.set('costume', 'ghost');
+
+//     const actual = makeUser(userData);
+
+//     expect.deepEqual(actual, expected);
+// });

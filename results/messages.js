5646dc4fd3ea05eb325b0c9fc6dbcc9fcdb99ce3
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-07-21 11:20:55 -0700

    building out results page

---
+++ results/messages.js
@@ -0,0 +1,17 @@
+export const aliveCandyMessages = {
+    skinny: 'your sugar buzz is almost nonexistent',
+    average: 'your buzz is almost as good as your buddy Tommys and you run around in circles for 10 minutes',
+    chunky: 'you are a wizard among your peers, you got the most candy and the biggest buzz - congratulations!'
+};
+
+export const deadCandyMessages = {
+    skinny: 'you were unable to collect enough candy to satisfy a newt',
+    average: 'you got enough candy to get your buzz on for a couple days!',
+    chunky: 'you collected enough candy to last you a month, your family is proud!'
+};
+
+export const healthMessages = {
+    dead: 'you died!',
+    okay: 'you did alright tonight, enjoy your candy!',
+    chunky: 'you got so much candy your parents will have to get you some new clothes!',
+};

004a4e2664a406907c222f47a14040480b870214
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-07-23 15:11:55 -0700

    working test

--- data/storage-utils.js
+++ data/storage-utils.js
@@ -1,4 +1,4 @@
-const USER = 'USER';
+export const USER = 'USER';
 
 export function getUser(){
     const userString = localStorage.getItem(USER);
@@ -9,3 +9,25 @@ export function setUser(userObject){
     const userString = JSON.stringify(userObject);
     localStorage.setItem(USER, userString);
 }
+
+export function findById(items, id) {
+    for (const item of items) {
+        if (item.id === id) {
+            return item;
+        }
+    }
+}
+
+// export function loadUser() {
+//     const name = document.getElementById('name');
+//     const health = document.getElementById('health');
+//     const candy = document.getElementById('candy');
+
+//     const user = getUser();
+
+//     name.textContent = `Name: ${user.name}`;
+//     health.textContent = `Health: ${user.health}`;
+//     candy.textContent = `Candy: ${user.candy}`;
+
+//     if 
+// }
